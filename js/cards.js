2be791b2b169ae8d4a50543550c0b0f1d0a6d4d7
Nuno Das Neves <ndasneves@gmail.com>
2021-11-25 13:59:06 -0800

    cards

---
+++ js/cards.js
@@ -0,0 +1,57 @@
+
+const NUM_CARDS_PER_DECK = 54;
+const NUM_JOKERS_PER_DECK = 2;
+const SUITS = ["Diamonds", "Hearts", "Clubs", "Spades"];
+
+const VALUE_TO_CARD_NAME={
+    1: "Ace",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "10",
+    11: "Jack",
+    12: "Queen",
+    13: "King",
+    14: "Joker",
+};
+
+const WILD_VALUES = [13,14];
+
+function Card(value, suite) {
+    this.value = value;
+    this.suite = suite;
+    this.wild = WILD_VALUES.includes(value);
+    this.string = value == 14 ? "Joker" : `${VALUE_TO_CARD_NAME[value]} of ${SUITS[suite]}`;
+}
+
+const DECK = Object.keys(VALUE_TO_CARD_NAME)
+                .map(val => Number(val))
+                .filter((val) => val != 14) // omit jokers
+                .flatMap((val) => (Array.from(SUITS, (_,s) => new Card(val,s))))
+                .concat(Array.from(Array(NUM_JOKERS_PER_DECK), () => new Card(14,0)));
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function shuffleArray(a) {
+    var n = a.length;
+    while (n > 0) {
+        var p = getRandomInt(n);
+        n--;
+        /* swap */
+        var tmp = a[n];
+        a[n] = a[p];
+        a[p] = tmp;
+    }
+    return a;
+}
+
+function makeDecks(n) {
+    return Array.from(Array(n), () => DECK).flat();
+}
